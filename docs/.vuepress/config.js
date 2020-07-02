bba132c54df17c1d34a6508b40c25d30dc4aa227
root <root@DESKTOP-2AE2J65.localdomain>
2020-07-02 21:55:07 +0800

    add .travis.yml

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   title: "天街小雨",
   description: 'Fake It Till You Make It!',
-  dest: 'public',
+  // dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
