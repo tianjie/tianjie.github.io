69c5d43996a7519ba5d27a7011e652eefbb4a7f4
root <root@DESKTOP-2AE2J65.localdomain>
2020-07-02 22:18:14 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   title: "天街小雨",
   description: 'Fake It Till You Make It!',
-  // dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
